e5b76ba8e27c11b64022b70b993a379993525cdb
Landon Key <>
2024-02-15 14:37:28 -0600

    cache and training folders

--- run.sh
+++ run.sh
@@ -3,6 +3,9 @@ cd "$(dirname "$0")"
 
 docker build -f Dockerfile . -t ghost-onnx-export:latest
 docker run -it --shm-size=2gb --gpus all \
+    -v ./docker_models_cache/.cache/:/root/.cache/ \
+    -v ./docker_models_cache/.insightface/:/root/.insightface/ \
+    -v ./docker_models_cache/.superres/:/root/.superres/ \
     -v /mnt/d/TrainingData/sdxl_turbo_faces/:/sdxl_turbo_faces \
     -v /mnt/d/TrainingData/lfw_funneled/ALL:/lfw_funneled \
     -v /mnt/d/TrainingData/lfw_funneled/CROP:/lfw_funneled_crop \
@@ -15,5 +18,7 @@ docker run -it --shm-size=2gb --gpus all \
     -v /mnt/d/TrainingData/FromBadges/raw:/frombadges \
     -v /mnt/d/TrainingData/FromBadges/crop:/frombadges_crop \
     -v /mnt/d/TrainingData/real_faces_128:/real_faces_128 \
+    -v /mnt/d/TrainingData/sdxl_celeba_mix/raw/:/sdxl_celeba_mix \
+    -v /mnt/d/TrainingData/sdxl_celeba_mix/crop/:/sdxl_celeba_mix_crop \
     -v ./output:/app/ghost/output/ \
     ghost-onnx-export:latest
